ca4192704adb68e7a80b7b673e1ea2772efc3ffc
Егор Романович <Egor_rom13@mail.ru>
2025-10-26 17:13:03 +0500

    Task5

--- Tyuiu.RomanovichEN.Sprint3.Task5.V10.Test/DataServiceTest.cs
+++ Tyuiu.RomanovichEN.Sprint3.Task5.V10.Test/DataServiceTest.cs
@@ -8,6 +8,10 @@ namespace Tyuiu.RomanovichEN.Sprint3.Task5.V10.Test
         [TestMethod]
         public void ValidGetSumSumSeries()
         {
+            DataService ds = new DataService();
+            double wait = 20685;
+            double res = ds.GetSumSumSeries(5, 1, 1, 3, 10);
+            Assert.AreEqual(wait, res);
         }
     }
 }
