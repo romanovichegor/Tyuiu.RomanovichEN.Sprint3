d15557075042a66fe99146c1e68d080e78a70259
Егор Романович <Egor_rom13@mail.ru>
2025-10-26 11:15:09 +0500

    Task3

---
+++ Tyuiu.RomanovichEN.Sprint3.Task3.V20.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using Tyuiu.RomanovichEN.Sprint3.Task3.V20.Lib;
+
+namespace Tyuiu.RomanovichEN.Sprint3.Task3.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCharCount()
+        {
+            DataService ds = new DataService();
+            string str = "gfft ntf f opf";
+            int wait = 5;
+            char f = 'f';
+            int res = ds.GetCharCount(str, f);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
